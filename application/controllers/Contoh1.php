ae78eff116ebd511ba906a0682eb6e08bf3075dc
virgijr <12200479@bsi.ac.id>
2021-10-05 16:32:20 +0700

    pustaka-booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php 
+class Contoh1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Virgiawan Tri Saputra Saya tingga di daerah Bogor olah raga yang saya sukai adalah Sepak Bola"; 
+    } 
+}
